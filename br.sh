9eccba96b234cf8a5bd7a507afe0ce322bf9167c
Rnt <kvrenat@gmail.com>
2016-10-12 11:34:49 +0600

    check max value

--- br.sh
+++ br.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 shopt -s extglob;
+dir_path="/sys/class/backlight/intel_backlight"
+
 case $1 in
     +([0-9]) ) br=$1 ;;
     * ) br=0 ;;
 esac
 
-if [ $br -gt 100 ]; then
-    echo "$br" > /sys/class/backlight/intel_backlight/brightness;
+max_br=`cat $dir_path/max_brightness`;
+
+if (($br > 100)) && (( $br < $max_br )) && [ -e "$dir_path/max_brightness" ]; then
+    echo "$br" > "$dir_path/brightness";
 else
     echo -n "May be wrong or danger parameter. Current value: ";
-    cat /sys/class/backlight/intel_backlight/brightness;
-fi
+    cat "$dir_path/brightness";
+fi 
